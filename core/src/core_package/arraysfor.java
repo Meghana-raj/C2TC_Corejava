2ac648369be0e127b3089f59536032502756a3d3
meghanaraj <meghanaraj@localhost>
2022-06-02 15:57:23 +0530

    june 2

---
+++ core/src/core_package/arraysfor.java
@@ -0,0 +1,20 @@
+package core_package;
+
+public class arraysfor {
+	
+		public static void main(String[] args) {
+		 
+		int array[] = {10, 20, 30, 40, 50};
+		
+		int sum = 0;
+		
+		
+		for (int i=0;i<array.length;i++)
+		{
+		sum += array[i]; 
+		}
+		
+		System.out. println("Sum of array elements=" + sum);
+		}
+
+}
